c95dd1156ae1b117ab1d3b5fed9429a6bebc1b89
AlexeyOleskevich <leshek2002@gmail.com>
2023-07-05 20:41:43 +0300

    Added Task08

---
+++ Task08/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.Write("Input a positive number greater than 1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string answer = "";
+if (number > 1)
+{
+    for (int i = 2; i <= number; i += 2)
+    {
+        answer += i + "";
+        if (i >= number - 1) break;
+        else answer += ", ";
+    }
+}
+else
+{
+    answer = "Wrong input!";
+}
+Console.Write(answer);
