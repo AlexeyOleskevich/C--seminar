5d8a01f8dcb364cc769d4ab355e38ce7bc29df6a
AlexeyOleskevich <leshek2002@gmail.com>
2023-07-11 22:57:12 +0300

    Added tasks 17-23

---
+++ Task19/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 0) number = -number;
+if (IsFiveDigitNumber(number))
+{
+    bool isPalindrome = IsPalindrome(number);
+    Console.Write(isPalindrome ? "Да" : "Нет");
+}
+else
+{
+    Console.Write("Введённое число не является пятизначным");
+}
+
+bool IsPalindrome(int num)
+{
+    int firstDigit = num / 10000 % 10;
+    int secondDigit = num / 1000 % 10;
+    int fourthDigit = num / 10 % 10;
+    int fifthDigit = num % 10;
+
+    return firstDigit == fifthDigit && secondDigit == fourthDigit;
+}
+
+bool IsFiveDigitNumber(int num)
+{
+    return number >= 10000 && number <= 99999;
+}
+
---
+++ Task21/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A(3, 6, 8); B(2, 1, -7), -> 15.84
+// A(7, -5, 0); B(1, -1, 9)-> 11.53
+
+Console.WriteLine("Введите координаты точки A: ");
+Console.Write("x: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B: ");
+Console.Write("x: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double dist = GetDistance(x1, x2, y1, y2, z1, z2);
+double distRound = Math.Round(dist, 2, MidpointRounding.ToZero);
+
+Console.Write("Расстояние между A и B: " + distRound);
+
+double GetDistance(int xCoordinateA, int xCoordinateB, int yCoordinateA, int yCoordinateB, int zCoordinateA, int zCoordinateB)
+{
+    double xDifference = xCoordinateB - xCoordinateA;
+    double yDifference = yCoordinateB - yCoordinateA;
+    double zDifference = zCoordinateB - zCoordinateA;
+
+    double distance = Math.Sqrt(xDifference * xDifference + yDifference * yDifference + zDifference * zDifference);
+    return distance;
+}
